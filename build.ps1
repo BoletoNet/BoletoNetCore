f6e0801b537ddf28c17aa5f8097543f89a6a101f
Carlos dos Santos <cds@cds-software.com.br>
2019-07-24 11:39:19 -0300

    myget

--- build.ps1
+++ build.ps1
@@ -3,14 +3,12 @@ $buildNumber = $env:APPVEYOR_BUILD_NUMBER
 $solutionFile = "$rootDir\BoletoNetCore\BoletoNetCore.csproj"
 $solutionTest = "$rootDir\BoletoNetCore.Testes\BoletoNetCore.Testes.csproj"
 $nuspecPath = "$rootDir\BoletoNetCore\BoletoNetCore.nuspec"
-$nupkgPath = "$rootDir\NuGet"
 
 [xml]$xml = cat $nuspecPath
 $xml.package.metadata.version="3.0.0."+"$buildNumber"
 $xml.Save($nuspecPath)
 
-[xml]$xml = cat $nuspecPath
-$nupkgPath = $nupkgPath -f $xml.package.metadata.version
+$nupkgPath = "$rootDir\NuGet\$nuspecPath"
 
 <#dotnet test $solutionTest#>
 dotnet pack -c Release $solutionFile -o $nupkgPath
