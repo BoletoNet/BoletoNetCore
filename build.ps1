4e893773cea3214804157f86a5cbdc4bcbab5a14
Carlos dos Santos <cds@cds-software.com.br>
2019-07-24 11:52:12 -0300

    myget

--- build.ps1
+++ build.ps1
@@ -1,15 +1,17 @@
-$rootDir = $env:APPVEYOR_BUILD_FOLDER
-$buildNumber = $env:APPVEYOR_BUILD_NUMBER
+# $rootDir = $env:APPVEYOR_BUILD_FOLDER
+# $buildNumber = $env:APPVEYOR_BUILD_NUMBER
+$rootdir = "D:\cds\git\GitHub\boletonet\BoletoNetCore"
+$buildNumber = "9"
+
 $solutionFile = "$rootDir\BoletoNetCore\BoletoNetCore.csproj"
 $solutionTest = "$rootDir\BoletoNetCore.Testes\BoletoNetCore.Testes.csproj"
 $nuspecPath = "$rootDir\BoletoNetCore\BoletoNetCore.nuspec"
+$nupkgPath = "$rootDir\NuGet\"
 
 [xml]$xml = cat $nuspecPath
 $xml.package.metadata.version="3.0.0."+"$buildNumber"
 $xml.Save($nuspecPath)
 
-$nupkgPath = "$rootDir\NuGet\$nuspecPath"
-
 <#dotnet test $solutionTest#>
-dotnet pack -c Release $solutionFile -o $nupkgPath
-appveyor PushArtifact $nupkgPath
+dotnet pack -c Release $solutionFile /p:NuspecFile=$nuspecPath -o $nupkgPath
+# appveyor PushArtifact $nupkgPath
