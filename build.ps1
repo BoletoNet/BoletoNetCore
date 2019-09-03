f235beab6a4b61a8ebe355e918efb121992465f1
Carlos dos Santos <cds@cds-software.com.br>
2019-09-03 16:14:19 -0300

    criado pacote nuget PDF - Issue #33

--- build.ps1
+++ build.ps1
@@ -1,6 +1,7 @@
 $rootDir = $env:APPVEYOR_BUILD_FOLDER
 $buildNumber = $env:APPVEYOR_BUILD_NUMBER
 
+<#Pacote Principal#>
 $solutionFile = "$rootDir\BoletoNetCore\BoletoNetCore.csproj"
 $solutionTest = "$rootDir\BoletoNetCore.Testes\BoletoNetCore.Testes.csproj"
 $nuspecPath = "$rootDir\BoletoNetCore\BoletoNetCore.nuspec"
@@ -10,6 +11,17 @@ $nupkgPath = "$rootDir\NuGet\"
 $xml.package.metadata.version="3.0.1."+"$buildNumber"
 $xml.Save($nuspecPath)
 
-<#dotnet test $solutionTest#>
 dotnet pack -c Release $solutionFile /p:NuspecFile=$nuspecPath -o $nupkgPath
 appveyor PushArtifact $nupkgPath
+
+<#Pacote PDF#>
+$solutionFilePDF = "$rootDir\BoletoNetCore.PDF\BoletoNetCore.PDF.csproj"
+$nuspecPathPDF = "$rootDir\BoletoNetCore.PDF\BoletoNetCore.PDF.nuspec"
+$nupkgPathPDF = "$rootDir\NuGet.PDF\"
+
+[xml]$xmlPDF = cat $nuspecPathPDF
+$xmlPDF.package.metadata.version="3.0.1."+"$buildNumber"
+$xmlPDF.Save($nuspecPathPDF)
+
+dotnet pack -c Release $solutionFilePDF /p:NuspecFile=$nuspecPathPDF -o $nupkgPathPDF
+appveyor PushArtifact $nupkgPathPDF
