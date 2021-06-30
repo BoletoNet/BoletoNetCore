2eaf4bf0a54f14b46997bf7d696882625cefa712
Marciano Bandeira <marcianobandeira@gmail.com>
2021-06-30 00:04:31 -0300

    criado BancoSicredi.Online para implementar o registro online do boleto

---
+++ BoletoNetCore/Banco/Sicredi/BancoSicredi.Online.cs
@@ -0,0 +1,15 @@
+namespace BoletoNetCore
+{
+    partial class BancoSicredi : IBancoOnlineRest
+    {
+        public string GerarToken()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RegistrarBoleto(ref Boleto boleto, string registro)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
