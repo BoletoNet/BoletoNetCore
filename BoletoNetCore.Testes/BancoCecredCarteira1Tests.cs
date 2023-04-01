7aecbaf562b892ce5c50acc3b582f7986c903bb7
Fernando Calheiros <nando.calheirosx@gmail.com>
2023-04-01 20:46:32 -0300

    [REFATORAÇÃO] - Separação de métodos nos testes da carteira 01 Cecred

--- BoletoNetCore.Testes/BancoCecredCarteira1Tests.cs
+++ BoletoNetCore.Testes/BancoCecredCarteira1Tests.cs
@@ -47,7 +47,66 @@ namespace BoletoNetCore.Testes
         [TestCase(200, "74660", "02315602/74660", "7", "02315602000074660", "08597875700000200001011040231560200007466001", "08591.01107 40231.560208 00074.660010 7 87570000020000", 2021, 9, 28)]
         [TestCase(2100.5, "74669", "02315602/74669", "8", "02315602000074669", "08598875700002100501011040231560200007466901", "08591.01107 40231.560208 00074.669011 8 87570000210050", 2021, 9, 28)]
         [TestCase(89.9, "74603", "02315602/74603", "9", "02315602000074603", "08599875700000089901011040231560200007460301", "08591.01107 40231.560208 00074.603010 9 87570000008990", 2021, 9, 28)]
-        public void Cecred_1_BoletoOK(decimal valorTitulo, string nossoNumero, string numeroDocumento, string digitoVerificador, string nossoNumeroFormatado, string codigoDeBarras, string linhaDigitavel, params int[] anoMesDia)
+        public void Deve_criar_boleto_cecred_01_com_digito_verificador_valido(decimal valorTitulo, string nossoNumero, string numeroDocumento, string digitoVerificador, string nossoNumeroFormatado, string codigoDeBarras, string linhaDigitavel, params int[] anoMesDia)
+        {
+            //Ambiente
+            var boleto = new Boleto(_banco)
+            {
+                DataVencimento = new DateTime(anoMesDia[0], anoMesDia[1], anoMesDia[2]),
+                ValorTitulo = valorTitulo,
+                NossoNumero = nossoNumero,
+                NumeroDocumento = numeroDocumento,
+                EspecieDocumento = TipoEspecieDocumento.DM,
+                Pagador = Utils.GerarPagador()
+            };
+
+            //Ação
+            boleto.ValidarDados();
+
+            //Assertivas
+            Assert.That(boleto.CodigoBarra.DigitoVerificador, Is.EqualTo(digitoVerificador), $"Dígito Verificador diferente de {digitoVerificador}"); 
+        }
+
+        [TestCase(80.21, "74644", "02315602/74644", "1", "02315602000074644", "08591875700000080211011040231560200007464401", "08591.01107 40231.560208 00074.644014 1 87570000008021", 2021, 9, 28)]
+        [TestCase(187.25, "74641", "02315602/74641", "2", "02315602000074641", "08592875700000187251011040231560200007464101", "08591.01107 40231.560208 00074.641010 2 87570000018725", 2021, 9, 28)]
+        [TestCase(49, "75743", "02315602/75743", "3", "02315602000075743", "08593875700000049001011040231560200007574301", "08591.01107 40231.560208 00075.743013 3 87570000004900", 2021, 9, 28)]
+        [TestCase(105, "74601", "02315602/74601", "4", "02315602000074601", "08594875700000105001011040231560200007460101", "08591.01107 40231.560208 00074.601014 4 87570000010500", 2021, 9, 28)]
+        [TestCase(19.9, "74621", "02315602/74621", "5", "02315602000074621", "08595875700000019901011040231560200007462101", "08591.01107 40231.560208 00074.621012 5 87570000001990", 2021, 9, 28)]
+        [TestCase(110, "74643", "02315602/74643", "6", "02315602000074643", "08596875700000110001011040231560200007464301", "08591.01107 40231.560208 00074.643016 6 87570000011000", 2021, 9, 28)]
+        [TestCase(200, "74660", "02315602/74660", "7", "02315602000074660", "08597875700000200001011040231560200007466001", "08591.01107 40231.560208 00074.660010 7 87570000020000", 2021, 9, 28)]
+        [TestCase(2100.5, "74669", "02315602/74669", "8", "02315602000074669", "08598875700002100501011040231560200007466901", "08591.01107 40231.560208 00074.669011 8 87570000210050", 2021, 9, 28)]
+        [TestCase(89.9, "74603", "02315602/74603", "9", "02315602000074603", "08599875700000089901011040231560200007460301", "08591.01107 40231.560208 00074.603010 9 87570000008990", 2021, 9, 28)]
+        public void Deve_criar_boleto_cecred_01_com_nosso_numero_valido(decimal valorTitulo, string nossoNumero, string numeroDocumento, string digitoVerificador, string nossoNumeroFormatado, string codigoDeBarras, string linhaDigitavel, params int[] anoMesDia)
+        {
+            //Ambiente
+            var boleto = new Boleto(_banco)
+            {
+                DataVencimento = new DateTime(anoMesDia[0], anoMesDia[1], anoMesDia[2]),
+                ValorTitulo = valorTitulo,
+                NossoNumero = nossoNumero,
+                NumeroDocumento = numeroDocumento,
+                EspecieDocumento = TipoEspecieDocumento.DM,
+                Pagador = Utils.GerarPagador()
+            };
+
+            //Ação
+            boleto.ValidarDados();
+
+            //Assertivas
+            Assert.That(boleto.NossoNumero, Is.EqualTo(nossoNumeroFormatado), "Nosso número inválido"); 
+        }
+
+
+        [TestCase(80.21, "74644", "02315602/74644", "1", "02315602000074644", "08591875700000080211011040231560200007464401", "08591.01107 40231.560208 00074.644014 1 87570000008021", 2021, 9, 28)]
+        [TestCase(187.25, "74641", "02315602/74641", "2", "02315602000074641", "08592875700000187251011040231560200007464101", "08591.01107 40231.560208 00074.641010 2 87570000018725", 2021, 9, 28)]
+        [TestCase(49, "75743", "02315602/75743", "3", "02315602000075743", "08593875700000049001011040231560200007574301", "08591.01107 40231.560208 00075.743013 3 87570000004900", 2021, 9, 28)]
+        [TestCase(105, "74601", "02315602/74601", "4", "02315602000074601", "08594875700000105001011040231560200007460101", "08591.01107 40231.560208 00074.601014 4 87570000010500", 2021, 9, 28)]
+        [TestCase(19.9, "74621", "02315602/74621", "5", "02315602000074621", "08595875700000019901011040231560200007462101", "08591.01107 40231.560208 00074.621012 5 87570000001990", 2021, 9, 28)]
+        [TestCase(110, "74643", "02315602/74643", "6", "02315602000074643", "08596875700000110001011040231560200007464301", "08591.01107 40231.560208 00074.643016 6 87570000011000", 2021, 9, 28)]
+        [TestCase(200, "74660", "02315602/74660", "7", "02315602000074660", "08597875700000200001011040231560200007466001", "08591.01107 40231.560208 00074.660010 7 87570000020000", 2021, 9, 28)]
+        [TestCase(2100.5, "74669", "02315602/74669", "8", "02315602000074669", "08598875700002100501011040231560200007466901", "08591.01107 40231.560208 00074.669011 8 87570000210050", 2021, 9, 28)]
+        [TestCase(89.9, "74603", "02315602/74603", "9", "02315602000074603", "08599875700000089901011040231560200007460301", "08591.01107 40231.560208 00074.603010 9 87570000008990", 2021, 9, 28)]
+        public void Deve_criar_boleto_cecred_01_com_codigo_de_barras_valido(decimal valorTitulo, string nossoNumero, string numeroDocumento, string digitoVerificador, string nossoNumeroFormatado, string codigoDeBarras, string linhaDigitavel, params int[] anoMesDia)
         {
             //Ambiente
             var boleto = new Boleto(_banco)
@@ -64,9 +123,35 @@ namespace BoletoNetCore.Testes
             boleto.ValidarDados();
 
             //Assertivas
-            Assert.That(boleto.CodigoBarra.DigitoVerificador, Is.EqualTo(digitoVerificador), $"Dígito Verificador diferente de {digitoVerificador}");
-            Assert.That(boleto.NossoNumero, Is.EqualTo(nossoNumeroFormatado), "Nosso número inválido");
             Assert.That(boleto.CodigoBarra.CodigoDeBarras, Is.EqualTo(codigoDeBarras), "Código de Barra inválido");
+        }
+
+
+        [TestCase(80.21, "74644", "02315602/74644", "1", "02315602000074644", "08591875700000080211011040231560200007464401", "08591.01107 40231.560208 00074.644014 1 87570000008021", 2021, 9, 28)]
+        [TestCase(187.25, "74641", "02315602/74641", "2", "02315602000074641", "08592875700000187251011040231560200007464101", "08591.01107 40231.560208 00074.641010 2 87570000018725", 2021, 9, 28)]
+        [TestCase(49, "75743", "02315602/75743", "3", "02315602000075743", "08593875700000049001011040231560200007574301", "08591.01107 40231.560208 00075.743013 3 87570000004900", 2021, 9, 28)]
+        [TestCase(105, "74601", "02315602/74601", "4", "02315602000074601", "08594875700000105001011040231560200007460101", "08591.01107 40231.560208 00074.601014 4 87570000010500", 2021, 9, 28)]
+        [TestCase(19.9, "74621", "02315602/74621", "5", "02315602000074621", "08595875700000019901011040231560200007462101", "08591.01107 40231.560208 00074.621012 5 87570000001990", 2021, 9, 28)]
+        [TestCase(110, "74643", "02315602/74643", "6", "02315602000074643", "08596875700000110001011040231560200007464301", "08591.01107 40231.560208 00074.643016 6 87570000011000", 2021, 9, 28)]
+        [TestCase(200, "74660", "02315602/74660", "7", "02315602000074660", "08597875700000200001011040231560200007466001", "08591.01107 40231.560208 00074.660010 7 87570000020000", 2021, 9, 28)]
+        [TestCase(2100.5, "74669", "02315602/74669", "8", "02315602000074669", "08598875700002100501011040231560200007466901", "08591.01107 40231.560208 00074.669011 8 87570000210050", 2021, 9, 28)]
+        [TestCase(89.9, "74603", "02315602/74603", "9", "02315602000074603", "08599875700000089901011040231560200007460301", "08591.01107 40231.560208 00074.603010 9 87570000008990", 2021, 9, 28)]
+        public void Deve_criar_boleto_cecred_01_com_linha_digitavel_valida(decimal valorTitulo, string nossoNumero, string numeroDocumento, string digitoVerificador, string nossoNumeroFormatado, string codigoDeBarras, string linhaDigitavel, params int[] anoMesDia)
+        {
+            //Ambiente
+            var boleto = new Boleto(_banco)
+            {
+                DataVencimento = new DateTime(anoMesDia[0], anoMesDia[1], anoMesDia[2]),
+                ValorTitulo = valorTitulo,
+                NossoNumero = nossoNumero,
+                NumeroDocumento = numeroDocumento,
+                EspecieDocumento = TipoEspecieDocumento.DM,
+                Pagador = Utils.GerarPagador()
+            };
+
+            //Ação
+            boleto.ValidarDados();
+             
             Assert.That(boleto.CodigoBarra.LinhaDigitavel, Is.EqualTo(linhaDigitavel), "Linha digitável inválida");
         }
     }
