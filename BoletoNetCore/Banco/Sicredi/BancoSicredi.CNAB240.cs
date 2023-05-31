3ea26268acdcf04f475e77345470b396314f4631
Anderson Reis de Campos <acampos@dpinet.com.br>
2023-05-31 10:20:52 -0300

    Padronização Espécie Documento CNAB240 SICREDI.

--- BoletoNetCore/Banco/Sicredi/BancoSicredi.CNAB240.cs
+++ BoletoNetCore/Banco/Sicredi/BancoSicredi.CNAB240.cs
@@ -133,7 +133,7 @@ namespace BoletoNetCore
             reg.Adicionar(TTiposDadoEDI.ediNumericoSemSeparador_, 0086, 015, 2, boleto.ValorTitulo, '0'); // 086 a 100 - Valor nominal do título
             reg.Adicionar(TTiposDadoEDI.ediAlphaAliDireita______, 0101, 005, 0, "00000", '0'); // 101 a 105 - Coop./Ag. encarregada da cobrança
             reg.Adicionar(TTiposDadoEDI.ediAlphaAliDireita______, 0106, 001, 0, "", ' '); // 106 a 106 - Dígito verificador da coop./agência
-            reg.Adicionar(TTiposDadoEDI.ediAlphaAliDireita______, 0107, 002, 0, this.EspecieDocumentoSicrediCNAB240(boleto.EspecieDocumento), ' '); // 107 a 108 - Espécie do título
+            reg.Adicionar(TTiposDadoEDI.ediNumericoSemSeparador_, 0107, 002, 0, (int)boleto.EspecieDocumento, '0'); // 107 a 108 - Espécie do título
             reg.Adicionar(TTiposDadoEDI.ediAlphaAliDireita______, 0109, 001, 0, boleto.Aceite, 'N'); // 109 a 109 - Identificação de título aceito/não aceito
             reg.Adicionar(TTiposDadoEDI.ediDataDDMMAAAA_________, 0110, 008, 0, boleto.DataEmissao, '0'); // 110 a 117 - Data da emissão do título
 
@@ -192,33 +192,6 @@ namespace BoletoNetCore
             reg.Adicionar(TTiposDadoEDI.ediNumericoSemSeparador_, 0240, 001, 0, "", ' '); // 240 a 240 - Uso exclusivo FEBRABAN/CNAB
             reg.CodificarLinha();
             return Utils.SubstituiCaracteresEspeciais(reg.LinhaRegistro);
-        }
-
-        private string EspecieDocumentoSicrediCNAB240(TipoEspecieDocumento EspecieDocumento)
-        {
-            switch (EspecieDocumento)
-            {
-                case TipoEspecieDocumento.DMI:
-                    return "03";
-                case TipoEspecieDocumento.DR:
-                    return "06";
-                case TipoEspecieDocumento.NP:
-                    return "12";
-                case TipoEspecieDocumento.NPR:
-                    return "13";
-                case TipoEspecieDocumento.NS:
-                    return "16";
-                case TipoEspecieDocumento.RC:
-                    return "17";
-                case TipoEspecieDocumento.LC:
-                    return "07";
-                case TipoEspecieDocumento.DSI:
-                    return "05";
-                case TipoEspecieDocumento.OU:
-                    return "99";
-            }
-
-            return "99"; // outros
         }
 
         public string GerarHeaderLoteRemessaCNAB240(ref int numeroArquivoRemessa, ref int numeroRegistro)
