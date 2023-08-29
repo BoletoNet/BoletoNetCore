2428097a98340f6398505528b19d6b5ba475c08e
Matheus Inacio Medeiros <matheus.medeiros@insidesistemas.com.br>
2023-08-29 14:24:18 -0300

    Corrigido versão do nuget.

--- build.ps1
+++ build.ps1
@@ -1,17 +1,16 @@
 $rootDir = $env:APPVEYOR_BUILD_FOLDER
 $buildVersion = $Env:APPVEYOR_BUILD_VERSION
-Write-Host "APPVEYOR_BUILD_VERSION: $buildVersion"
 
 <#Pacote Principal#>
 $solutionFile = Join-Path $rootDir "BoletoNetCore\BoletoNetCore.csproj"
 $nupkgPath = Join-Path $rootDir "NuGet"
 
 dotnet build -c Release $solutionFile /p:Version=$buildVersion
-dotnet pack -c Release $solutionFile -o $nupkgPath
+dotnet pack -c Release $solutionFile -o $nupkgPath /p:Version=$buildVersion
 
 <#Pacote PDF#>
 $solutionFilePDF = Join-Path $rootDir "BoletoNetCore.PDF\BoletoNetCore.PDF.csproj"
 $nupkgPathPDF = Join-Path $rootDir "NuGet.PDF"
 
 dotnet build -c Release $solutionFilePDF /p:Version=$buildVersion
-dotnet pack -c Release $solutionFilePDF -o $nupkgPathPDF
+dotnet pack -c Release $solutionFilePDF -o $nupkgPathPDF /p:Version=$buildVersion
