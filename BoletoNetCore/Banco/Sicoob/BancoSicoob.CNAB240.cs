adc9f15606ea8e7217ecdac46b660847e8612e38
Marcos Graeber <graeber02@hotmail.com>
2025-06-20 17:29:35 -0300

    ajuste numero parcela na remessa sicoob

--- BoletoNetCore/Banco/Sicoob/BancoSicoob.CNAB240.cs
+++ BoletoNetCore/Banco/Sicoob/BancoSicoob.CNAB240.cs
@@ -192,7 +192,7 @@ namespace BoletoNetCore
                     reg.Adicionar(TTiposDadoEDI.ediNumericoSemSeparador_, 0038, 009, 0, boleto.NossoNumero, '0');
                     reg.Adicionar(TTiposDadoEDI.ediNumericoSemSeparador_, 0047, 001, 0, boleto.NossoNumeroDV, '0');
                 }
-                reg.Adicionar(TTiposDadoEDI.ediNumericoSemSeparador_, 0048, 002, 0, "01", '0');
+                reg.Adicionar(TTiposDadoEDI.ediNumericoSemSeparador_, 0048, 002, 0, string.IsNullOrWhiteSpace(boleto.NumeroDocumento) ? "01" : boleto.NumeroDocumento, '0');
                 reg.Adicionar(TTiposDadoEDI.ediNumericoSemSeparador_, 0050, 002, 0, boleto.VariacaoCarteira, '0');
                 reg.Adicionar(TTiposDadoEDI.ediNumericoSemSeparador_, 0052, 001, 0, "4", '0');
                 reg.Adicionar(TTiposDadoEDI.ediAlphaAliEsquerda_____, 0053, 005, 0, Empty, ' ');
