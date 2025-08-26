c3d0b9f63c8a27e5e64459a14bc11b03146b2726
hardshop <hardshop@hardshop.com.br>
2025-08-26 08:26:35 -0300

    #373-Ajuste no CNAB 400 do Banco Safra para preencher ValorPago corretamente

--- BoletoNetCore/Banco/Safra/BancoSafra.CNAB400.cs
+++ BoletoNetCore/Banco/Safra/BancoSafra.CNAB400.cs
@@ -57,7 +57,7 @@ namespace BoletoNetCore
                 boleto.ValorIOF = Convert.ToDecimal(registro.Substring(214, 13)) / 100;
                 boleto.ValorAbatimento = Convert.ToDecimal(registro.Substring(227, 13)) / 100;
                 boleto.ValorDesconto = Convert.ToDecimal(registro.Substring(240, 13)) / 100;
-                boleto.ValorPagoCredito = Convert.ToDecimal(registro.Substring(253, 13)) / 100;
+                boleto.ValorPago = Convert.ToDecimal(registro.Substring(253, 13)) / 100;
                 boleto.ValorJurosDia = Convert.ToDecimal(registro.Substring(266, 13)) / 100;
                 boleto.ValorOutrosCreditos = Convert.ToDecimal(registro.Substring(279, 13)) / 100;
 
